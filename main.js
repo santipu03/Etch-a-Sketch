7e55718e8215a2990472aa6e43128cc3e0834c13
santipu_ <santipujolsau@gmail.com>
2022-02-28 20:45:27 +0100

    Added listener to the items so they color when mouse hover

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 const container = document.querySelector(".container");
 
+
 for (let i = 0; i < 256; i++){
     let div = document.createElement("div");
     div.classList.add("item");
     container.append(div);
 };
 
-
 container.style.gridTemplateColumns = "repeat(16, auto)";
 container.style.gridTemplateRows = "repeat(16, auto)";
+
+
+const items = document.querySelectorAll(".item");
+items.forEach(item => {
+    item.addEventListener("mouseover", ()=> {
+        item.style.backgroundColor = "black";
+    })
+})
+
+
